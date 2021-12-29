e6aa8a94e84303b3fa5a765e353b1cb31df1d452
brayanespitia <brayanespitia2013@gmaail.com>
2021-12-28 22:24:54 -0500

    commit 2

--- src/main/java/entity/Jugador.java
+++ src/main/java/entity/Jugador.java
@@ -1,4 +1,18 @@
 package entity;
 
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
 public class Jugador {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
