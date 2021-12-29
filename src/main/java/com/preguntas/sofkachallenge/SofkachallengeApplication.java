bc971e2738c7ed335910cbde73e71b6108b8cfc2
brayanespitia <brayanespitia2013@gmaail.com>
2021-12-28 21:54:11 -0500

    Initial commit

---
+++ src/main/java/com/preguntas/sofkachallenge/SofkachallengeApplication.java
@@ -0,0 +1,13 @@
+package com.preguntas.sofkachallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofkachallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofkachallengeApplication.class, args);
+	}
+
+}
